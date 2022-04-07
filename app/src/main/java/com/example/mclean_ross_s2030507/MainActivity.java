c8fc40685cc9f669a0a6aeaad08e078dccfd68a6
Ross McLean <ri.mcl@icloud.com>
2022-04-07 17:01:35 +0100

    Implement toolbar
    Store Traffic Scotland URLs

--- app/src/main/java/com/example/mclean_ross_s2030507/MainActivity.java
+++ app/src/main/java/com/example/mclean_ross_s2030507/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.mclean_ross_s2030507;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // Store Traffic Scotland links
+    private String roadworksUrl = "https://trafficscotland.org/rss/feeds/roadworks.aspx";
+    private String plannedRoadworksUrl = "https://trafficscotland.org/rss/feeds/plannedroadworks.aspx";
+    private String currentIncidentsUrl = "https://trafficscotland.org/rss/feeds/currentincidents.aspx";
+    private String urlValue;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Make toolbar actionBar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.toolbar_title);
+        setSupportActionBar(toolbar);
+
+
     }
 }
