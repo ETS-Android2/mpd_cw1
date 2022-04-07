c3d924f3ae79877497b9e1cb134aa474d909c091
Ross McLean <ri.mcl@icloud.com>
2022-04-07 18:58:42 +0100

    Implement initial Sidebar

--- app/src/main/java/com/example/mclean_ross_s2030507/MainActivity.java
+++ app/src/main/java/com/example/mclean_ross_s2030507/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.mclean_ross_s2030507;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
     // Store Traffic Scotland links
     private String roadworksUrl = "https://trafficscotland.org/rss/feeds/roadworks.aspx";
     private String plannedRoadworksUrl = "https://trafficscotland.org/rss/feeds/plannedroadworks.aspx";
     private String currentIncidentsUrl = "https://trafficscotland.org/rss/feeds/currentincidents.aspx";
     private String urlValue;
 
+    DrawerLayout drawerLayout;
+    ArrayList<ListComponent> componentsList;
 
 
     @Override
@@ -24,6 +36,33 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle(R.string.toolbar_title);
         setSupportActionBar(toolbar);
 
+        // Initialise componentsList
+        componentsList = new ArrayList<>();
 
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed () {
+        DrawerLayout drawer = findViewById(R.id. drawer_layout) ;
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed() ;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
